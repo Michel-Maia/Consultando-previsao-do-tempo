d0ec53882edefbbc2e381a36c7484e3a7be07d94
Michel Maia <programador.michel.maia@gmail.com>
2022-11-10 00:25:28 -0300

    commit

--- README.md
+++ README.md
@@ -1,2 +1,22 @@
 # Consultando previsão do tempo
- desenvolvido para aprendizado
+
+![preview](./.github/preview.png)
+
+
+Desenvolvido para aprendizado. Consulta de previsão do tempo. 
+
+
+[🔗 Clique aqui para acessar] (https://michel-maia.github.io/consultando-previsao-do-tempo/)
+
+
+## 💻 Tecnologias
+
+- HTML
+- CSS
+- Javascript
+
+
+## Referência
+
+- https://www.youtube.com/c/WillDev2022
+- https://openweathermap.org/
--- script.js
+++ script.js
@@ -15,9 +15,9 @@ button.addEventListener("click", () => {
 
 async function getDataApi() {
     let url = `https://api.openweathermap.org/data/2.5/weather?q=${encodeURI(
-        
         input.value
-    )}&units=metric&appid=e9af4048996d7749043ff01fd9e31b3e`;
+
+      )}&units=metric&appid=e9af4048996d7749043ff01fd9e31b3e`;
 
     try {
         await fetch(url)
@@ -35,11 +35,11 @@ async function getDataApi() {
 }
 
 function loadData(data) {
-    place.innerHTML = `${data.name}, ${data.sys.country}`;
-    degrees.innerHTML = `Temperatura: ${Math.floor(data.main.temp)}° C`;
-    img.src = `https://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
-    wind.innerHTML = `Vento: ${data.wind.speed} km/h`;
-    content.style.display = "flex";
+  place.innerHTML = `${data.name}, ${data.sys.country}`;
+  degrees.innerHTML = `Temperatura: ${Math.floor(data.main.temp)}° C`;
+  img.src = `http://openweathermap.org/img/wn/${data.weather[0].icon}@2x.png`;
+  wind.innerHTML = `Vento: ${data.wind.speed} km/h`;
+  content.style.display = "flex";
 }
 
 
